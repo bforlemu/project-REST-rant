fa2f84d2f0a49075991ed26b51c992ea9b698d2c
Bertold F <forlemub12@gmail.com>
2022-07-11 18:25:47 -0700

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -2,19 +2,19 @@ const router = require('express').Router()
 
 // GET /places
 app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/salad-bowel.jpg'
+  }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/meat-board.jpg'
+  }]  
       
       res.render('places/index', { places })
 
--- views/home.jsx
+++ views/home.jsx
@@ -3,16 +3,24 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+    <h1>HOME</h1>
+    <div>
+      <img src="/images/burger.jpg" alt="fat burger" />
+      <div>
+      Photo by <a href="https://unsplash.com/@1ncreased?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lidye</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
+
+        </Def>
     )
-  }
-  
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+}
 
 module.exports = home
+
+
