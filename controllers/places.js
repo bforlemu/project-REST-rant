fea5b807dbd22dbbcecce3d373a2f9645e81406a
Bertold F <forlemub12@gmail.com>
2022-07-11 19:21:13 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -44,5 +44,18 @@ router.post('/', (req, res) => {
 })
 
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+
+  }
+})
 
 
